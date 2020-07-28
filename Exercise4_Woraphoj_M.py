f1655412ea2af97ad1e7ced31201e1d793187cf5
kurobuta1479 <kurobuta1479@gmail.com>
2020-07-28 19:55:39 +0700

    Create Exercise4_Woraphoj_M.py

---
+++ Exercise4_Woraphoj_M.py
@@ -0,0 +1,14 @@
+fe = 75
+'Foundation English'
+gb = 70
+'General Business'
+ics = 80
+'Introduction to Computer Systems'
+cp = 85
+'Computer Programming'
+
+print("-------------- Your Score ----------------")
+print("Foundation English :                   ",fe)
+print("General Business :                     ",gb)
+print("Introduction to Computer Systems :     ",ics)
+print("Computer Programming :                 ",cp)
