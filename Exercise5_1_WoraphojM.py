f2f33f03edf865c8bb078b43728dfa81911705ea
kurobuta1479 <kurobuta1479@gmail.com>
2020-08-01 13:45:10 +0700

    Create Exercise5_1_WoraphojM.py

---
+++ Exercise5_1_WoraphojM.py
@@ -0,0 +1,11 @@
+numberA = float(500)
+numberB = float(100)
+plus = numberA+numberB
+minus = numberA-numberB
+multiple = numberA*numberB
+divis = numberA/numberB
+print(numberA,"+",numberB,"=",plus)
+print(numberA,"-",numberB,"=",minus)
+print(numberA,"*",numberB,"=",multiple)
+print(numberA,"/",numberB,"=",divis)
+
